74fc1cb61947323a06b1f78aacb5d7b91fb1cbe3
ashley-monaghan <ashley-monaghan@users.noreply.github.com>
2022-10-20 03:21:51 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@
 // This file contains the JS functions for index.html
 "use strict"
 /**
-* This function calculates you total salary and deducted.
-*/
+ * This function calculates you total salary and deducted.
+ */
 function calculate() {
-// input
-const hours = parseInt(document.getElementById("number-of-hours").value)
-const rate = parseInt(document.getElementById("hourly-rate").value)
-// process
-const totalPay = (hours * rate)
-// output
-document.getElementById("totalPay").innerHTML = "Your pay will be: $ " + hours + " "
+  // input
+  const hours = parseInt(document.getElementById("number-of-hours").value)
+  const rate = parseInt(document.getElementById("hourly-rate").value)
+  // process
+  const totalPay = hours * rate
+  // output
+  document.getElementById("totalPay").innerHTML =
+    "Your pay will be: $ " + hours + " "
 }
