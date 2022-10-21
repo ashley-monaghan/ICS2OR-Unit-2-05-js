4d56f6253d43ee7c98fff5ede4a9d5a47e082678
ashley-monaghan <113056162+ashley-monaghan@users.noreply.github.com>
2022-10-20 22:05:50 -0400

    parseFloat

--- js/script.js
+++ js/script.js
@@ -3,24 +3,25 @@
 // Created by: Ashley Monaghan
 // Created on: Oct 2022
 // This file contains the JS functions for index.html
+
 "use strict"
 /**
  * This function calculates you total salary and deducted.
  */
 function calculate() {
   // input
-  const hours = parseInt(document.getElementById("number-of-hours").value)
-  const rate = parseInt(document.getElementById("hourly-rate").value)
+  const hours = parseFloat(document.getElementById("number-of-hours").value)
+  const rate = parseFloat(document.getElementById("hourly-rate").value)
 
   // process
-  const totalPay = hours * rate
-  const taxDeductions = (hours * rate) / 0.18
+  const totalPay = hours * rate * (1 - 0.18)
+  const taxDeductions = hours * rate * 0.18
 
   // output
-  document.getElementById("pay").innerHTML =
-    "Your total pay will be: $" + pay.toFixed(2)
+  document.getElementById("pay").innerHTML = 
+  "Your total pay will be: $" + pay.toFixed(2)
 
   // output
-  document.getElementById("pay").innerHTML =
-    "Your deducted pay will be: $" + taxDeductions.toFixed(2)
+  document.getElementById("tax").innerHTML =
+    "Your deducted pay will be: $" + tax.toFixed(2)
 }
