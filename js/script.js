043d6dde9c0f9eaedd124d8b172aef619e582e45
ashley-monaghan <113056162+ashley-monaghan@users.noreply.github.com>
2022-10-20 20:54:38 -0400

    I'm stupid lmao

--- js/script.js
+++ js/script.js
@@ -13,8 +13,8 @@ function calculate() {
   const rate = parseInt(document.getElementById("hourly-rate").value)
 
   // process
-  const totalPay = hours * rate
-  const taxDeductions = hours * rate * Math.PI
+  const totalPay = (hours * rate)
+  const taxDeductions = (hours * rate) / 0.18
 
   // output
   document.getElementById(
